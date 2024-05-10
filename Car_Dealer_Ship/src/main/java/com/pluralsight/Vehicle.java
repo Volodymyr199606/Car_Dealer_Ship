e6eb897982b51148a6330c84cb98874e6c662f0e
Volodymyr199606 <166452639+Volodymyr199606@users.noreply.github.com>
2024-05-10 14:08:29 -0400

    Created classes and added constructor to DealerShip, instantieted ArrayList Vehicle object in the constructor, and also created stubs for the other methods

---
+++ Car_Dealer_Ship/src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private int id;
+    private int year;
+    private String make;
+    private String model;
+    private String type;
+    private String color;
+    private int mileage;
+    private double price;
+
+    // getters and setters for each field
+}
